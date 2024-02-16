b8a090a4cc149cc7a78811868605713e1d50db2a
afaq <afaqahmed0321@gmail.com>
2024-02-16 15:44:12 +0500

    ntwork feed

--- src/Network/Users/NetworkFeed/NetworkFeedAPi.js
+++ src/Network/Users/NetworkFeed/NetworkFeedAPi.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
 
 const NetWorkFeedApi = async (networkUserNo) => {
-  console.log(networkUserNo);
-  const apiUrl = 'https://snappromise.com:8080/getUserNetworkFeed?userNo=' + networkUserNo;
+  console.log(networkUserNo, "from networkAPI");
+  // const apiUrl = `https://snappromise.com:8080/getUserNetworkFeed?userNo=${networkUserNo}&visibility=private`;
 
-  try {
-    const response = await axios.get(apiUrl);
-    const data = response.data.promisesList; // Accessing the data property of the Axios response
-    // console.log(data, "Network Feed");
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return null; // Returning null in case of an error, you can adjust this as needed
-  }
+  await axios.get(`https://snappromise.com:8080/getUserNetworkFeed?userNo=${networkUserNo}&visibility=private`)
+    .then((response) => {
+      const data = response.data.promisesList; // Accessing the data property of the Axios response
+      console.log(data, "Network Feed");
+      return data;
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+      return null; // Returning null in case of an error, you can adjust this as needed
+    })
+
 };
 
 export default NetWorkFeedApi;
